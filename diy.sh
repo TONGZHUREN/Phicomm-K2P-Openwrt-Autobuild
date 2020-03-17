9e94de60ad7cb91a2bc3ac1f651f0cf6d075c63f
TONGZHUREN <32959720+TONGZHUREN@users.noreply.github.com>
2020-03-17 11:26:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,12 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# Add Luci-theme
+packages_path=package/openwrt-packages
+mkdir -p $packages_path
+git clone https://github.com/Apocalypsor/luci-app-smartdns $packages_path/luci-app-smartdns
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial $packages_path/luci-theme-atmaterial
+git clone https://github.com/frainzy1477/luci-app-clash $packages_path/luci-app-clash
+git clone https://github.com/vernesong/OpenClash $packages_path/luci-app-Openclash
+git clone https://github.com/rosywrt/luci-theme-rosy $packages_path/luci-theme-rosy
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  $packages_path/luci-theme-argon
